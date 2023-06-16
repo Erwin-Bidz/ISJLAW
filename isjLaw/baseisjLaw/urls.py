cca2d16766c431f12e89a254a195971f5c694205
Erwin-Bidz <100195579+Erwin-Bidz@users.noreply.github.com>
2023-06-16 11:51:33 +0100

    Add files via upload

--- isjLaw/baseisjLaw/urls.py
+++ isjLaw/baseisjLaw/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import livres_cpp_view, titres_cpp_view, Home, code_procedure_penale
+from .views import Home, code_procedure_penale, code_penal, constitution, LoginPage, RegisterPage
 from rest_framework import routers
 from .views import TexteViewSet, LivreViewSet, TitreViewSet, ChapitreViewSet, SectionViewSet, ArticleViewSet, AlineaViewSet
 
+from django.contrib.auth.views import LogoutView
 from django.conf.urls.static import static
 from isjLaw import settings
 
@@ -18,7 +19,10 @@ router.register('alineas', AlineaViewSet)
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('login/', LoginPage.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('code_penal/', code_penal, name='code_penal'),
     path('code_procedure_penale/', code_procedure_penale, name='code_procedure_penale'),
-    path('livrescpp/', livres_cpp_view, name='livrescpp'),
-    path('titrescpp/', titres_cpp_view, name='titrescpp')
+    path('constitution/', constitution, name='constitution'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- isjLaw/baseisjLaw/views.py
+++ isjLaw/baseisjLaw/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import viewsets
 
 from .models import Texte, Livre, Titre, Chapitre, Section, Article, Alinea
 from .serializers import TexteSerializer, LivreSerializer, TitreSerializer, ChapitreSerializer, SectionSerializer, ArticleSerializer, AlineaSerializer
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 import requests
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -15,6 +18,32 @@ class Home(ListView):
     model = Texte
     template_name = 'home.html'
     context_object_name = 'textes'
+
+class LoginPage(LoginView):
+    template_name = 'baseisjLaw/login.html'
+    fiels = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class RegisterPage(FormView):
+    template_name = 'baseisjLaw/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('constitution')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+        ##S'assurer ici que l'utilisateur est authentifié
+
+    #def get(self, *args, **kwargs):
+    #    if self.request.user.is_authenticated:
+    #        return redirect('home')
+    #    return super(RegisterPage, self).get(*args, **kwargs)
 
 class TexteViewSet(viewsets.ModelViewSet):
     
@@ -56,47 +85,36 @@ class AlineaViewSet(viewsets.ModelViewSet):
     
     queryset = Alinea.objects.all()
     serializer_class = AlineaSerializer
-    filterset_fields = ['article', 'numero']
+    filterset_fields = ['article', 'numero', 'libelle']
 
-def livres_cpp_view(request):
-    response = requests.get('http://127.0.0.1:8000/api/livres/?text=2&numero=')
-    if response.status_code == 200:
-        data = response.json()
-        context = {'data' : data}
-        return render(request, 'livrescpp.html', context)
-    else:
-        return render(request, 'error.html')
 
-#def titres_cpp_view(request):
-    #response = requests.get('http://127.0.0.1:8000/titres/?livre=1&numero=')
-#    if response.status_code == 200:
-#        data = response.json()
-#        context = {'data' : data}
-#        return render(request, 'titrescpp.html', context)
-#    else:
-#        return render(request, 'error.html')
 
-def titres_cpp_view(request):
-    TitreResponse = requests.get('http://127.0.0.1:8000/api/titres/?livre=1&numero=')
-    chapResponse = requests.get('http://127.0.0.1:8000/api/chapitres/')
-    secResponse = requests.get('http://127.0.0.1:8000/api/sections/')
-    articleResponse = requests.get('http://127.0.0.1:8000/api/articles/')
-    alineaResponse = requests.get('http://127.0.0.1:8000/api/alineas/')
-    if TitreResponse.status_code == 200 and chapResponse.status_code == 200:
-        data = TitreResponse.json()
-        chapters = chapResponse.json()
-        sections = secResponse.json()
-        articles = articleResponse.json()
-        alineas = alineaResponse.json()
-        context = {'data': data, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas}
-        return render(request, 'titrescpp.html', context)
-    else:
-        return render(request, 'error.html')
+# def code_penal(request):
+#     texteResponse = requests.get('http://127.0.0.1:8000/api/textes/?id=1')
+#     livreResponse = requests.get('http://127.0.0.1:8000/api/livres/?text=1&numero=')
+#     titreResponse = requests.get('http://127.0.0.1:8000/api/titres/')
+#     chapResponse = requests.get('http://127.0.0.1:8000/api/chapitres/')
+#     secResponse = requests.get('http://127.0.0.1:8000/api/sections/')
+#     articleResponse = requests.get('http://127.0.0.1:8000/api/articles/')
+#     alineaResponse = requests.get('http://127.0.0.1:8000/api/alineas/')
+#     if texteResponse.status_code == 200 and livreResponse.status_code == 200 and titreResponse.status_code == 200 and chapResponse.status_code == 200 and secResponse.status_code == 200 and articleResponse.status_code == 200 and alineaResponse.status_code == 200:
+#         texte = texteResponse.json()
+#         livres = livreResponse.json()
+#         titres = titreResponse.json()
+#         chapters = chapResponse.json()
+#         sections = secResponse.json()
+#         articles = articleResponse.json()
+#         alineas = alineaResponse.json()
+#         context = {'texte': texte, 'livres': livres, 'titres': titres, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas}
+#         return render(request, 'cp.html', context)
+#     else:
+#         return render(request, 'error.html')
 
-def code_procedure_penale(request):
-    texteResponse = requests.get('http://127.0.0.1:8000/api/textes/?id=2')
-    livreResponse = requests.get('http://127.0.0.1:8000/api/livres/?text=2&numero=')
-    titreResponse = requests.get('http://127.0.0.1:8000/api/titres/?livre=1&numero=')
+def code_penal(request):
+    search_term = request.POST.get('search_term')
+    texteResponse = requests.get('http://127.0.0.1:8000/api/textes/?id=1')
+    livreResponse = requests.get('http://127.0.0.1:8000/api/livres/?text=1&numero=')
+    titreResponse = requests.get('http://127.0.0.1:8000/api/titres/')
     chapResponse = requests.get('http://127.0.0.1:8000/api/chapitres/')
     secResponse = requests.get('http://127.0.0.1:8000/api/sections/')
     articleResponse = requests.get('http://127.0.0.1:8000/api/articles/')
@@ -109,9 +127,54 @@ def code_procedure_penale(request):
         sections = secResponse.json()
         articles = articleResponse.json()
         alineas = alineaResponse.json()
-        context = {'texte': texte, 'livres': livres, 'titres': titres, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas}
+        context = {'texte': texte, 'livres': livres, 'titres': titres, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas, 'search_term': search_term}
+        return render(request, 'cp.html', context)
+    else:
+        return render(request, 'error.html')
+
+def code_procedure_penale(request):
+    search_term = request.POST.get('search_term')
+    if search_term is None:
+        search_term = ' '
+
+    texteResponse = requests.get('http://127.0.0.1:8000/api/textes/?id=2')
+    livreResponse = requests.get('http://127.0.0.1:8000/api/livres/?text=2&numero=')
+    titreResponse = requests.get('http://127.0.0.1:8000/api/titres/')
+    chapResponse = requests.get('http://127.0.0.1:8000/api/chapitres/')
+    secResponse = requests.get('http://127.0.0.1:8000/api/sections/')
+    articleResponse = requests.get('http://127.0.0.1:8000/api/articles/')
+    alineaResponse = requests.get('http://127.0.0.1:8000/api/alineas/')
+    if texteResponse.status_code == 200 and livreResponse.status_code == 200 and titreResponse.status_code == 200 and chapResponse.status_code == 200 and secResponse.status_code == 200 and articleResponse.status_code == 200 and alineaResponse.status_code == 200:
+        texte = texteResponse.json()
+        livres = livreResponse.json()
+        titres = titreResponse.json()
+        chapters = chapResponse.json()
+        sections = secResponse.json()
+        articles = articleResponse.json()
+        alineas = alineaResponse.json()
+        context = {'texte': texte, 'livres': livres, 'titres': titres, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas, 'search_term': search_term}
         return render(request, 'cpp.html', context)
     else:
         return render(request, 'error.html')
 
-
+def constitution(request):
+    search_term = request.POST.get('search_term')
+    texteResponse = requests.get('http://127.0.0.1:8000/api/textes/?id=3')
+    livreResponse = requests.get('http://127.0.0.1:8000/api/livres/?text=3&numero=')
+    titreResponse = requests.get('http://127.0.0.1:8000/api/titres/')
+    chapResponse = requests.get('http://127.0.0.1:8000/api/chapitres/')
+    secResponse = requests.get('http://127.0.0.1:8000/api/sections/')
+    articleResponse = requests.get('http://127.0.0.1:8000/api/articles/')
+    alineaResponse = requests.get('http://127.0.0.1:8000/api/alineas/')
+    if texteResponse.status_code == 200 and livreResponse.status_code == 200 and titreResponse.status_code == 200 and chapResponse.status_code == 200 and secResponse.status_code == 200 and articleResponse.status_code == 200 and alineaResponse.status_code == 200:
+        texte = texteResponse.json()
+        livres = livreResponse.json()
+        titres = titreResponse.json()
+        chapters = chapResponse.json()
+        sections = secResponse.json()
+        articles = articleResponse.json()
+        alineas = alineaResponse.json()
+        context = {'texte': texte, 'livres': livres, 'titres': titres, 'chapters': chapters, 'sections': sections, 'articles': articles, 'alineas': alineas, 'search_term': search_term}
+        return render(request, 'constitution.html', context)
+    else:
+        return render(request, 'error.html')
